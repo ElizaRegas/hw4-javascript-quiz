e3d4c787093570130d6277c2775a812aed553eb6
Eliza Regas <elizaregas@gmail.com>
2020-06-05 13:05:00 -0400

    question 1 information being pushed to html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,19 @@
 var startQuizEl = document.getElementById('startQuiz');
-var quiz = document.getElementById('quiz');
-var qImg = document.getElementById('qImage');
-var question = document.getElementById('question');
-var counter = document.getElementById('counter');
-var timeGauge = document.getElementById('timeGauge');
-var choiceA = document.getElementById('A');
-var choiceB = document.getElementById('B');
-var choiceC = document.getElementById('C');
-var progress = document.getElementById('progress');
-var scoreContainer = document.getElementById('scoreContainer');
+var quizEl = document.getElementById('quiz');
+var questionImageEl = document.getElementById('questionImage');
+var questionEl = document.getElementById('question');
+var counterEl = document.getElementById('counter');
+var timerEl = document.getElementById('timer');
+var choiceAEl = document.getElementById('A');
+var choiceBEl = document.getElementById('B');
+var choiceCEl = document.getElementById('C');
+var progressEl = document.getElementById('progress');
+var scoreContainerEl = document.getElementById('scoreContainer');
 
-var questions = [
+var questionsObj = [
   {
-    question : "Question 1",
-    imgSrc : "./assets/images/lizpf2.jpg",
+    question : "Question #1:",
+    imageSrc : "./assets/images/lizpf2.jpg",
     choiceA : "Option 1A",
     choiceB : "Option 1B",
     choiceC : "Option 1C",
@@ -21,7 +21,7 @@ var questions = [
   },
   {
     question : "Question 2",
-    imgSrc : "",
+    imageSrc : "",
     choiceA : "Option 2A",
     choiceB : "Option 2B",
     choiceC : "Option 2C",
@@ -29,7 +29,7 @@ var questions = [
   },
   {
     question : "Question 3",
-    imgSrc : "",
+    imageSrc : "",
     choiceA : "Option 3A",
     choiceB : "Option 3B",
     choiceC : "Option 3C",
@@ -37,25 +37,21 @@ var questions = [
   },
 ]
 
-var questionIndex = questions.length;
-var lastQuestionIndex = questionIndex - 1;
-var runningQuestionIndex = 0;
+var lastQuestionIndex = questionsObj.length - 1;
+var currentQuestionIndex = 0;
 
 // starting the game by clicking on Start Quiz
-document.addEventListener("click", renderQuestion);
+document.addEventListener("click", displayQuestion);
 
 // display question
-function renderQuestion(){
-  let q = questions[runningQuestionIndex];
-  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-  // question.innerHTML = q.question;
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
-  runningQuestionIndex ++;
+function displayQuestion(){
+  var quest = questionsObj[currentQuestionIndex];
+  questionImageEl.innerHTML = "<img src=" + quest.imageSrc + ">";
+  question.innerHTML = quest.question;
+  choiceAEl.innerHTML = quest.choiceA;
+  choiceBEl.innerHTML = quest.choiceB;
+  choiceCEl.innerHTML = quest.choiceC;
+  currentQuestionIndex ++;
+  console.log("display question run");
 }
-
-
-renderQuestion();
-console.log('questioned rendered');
 
